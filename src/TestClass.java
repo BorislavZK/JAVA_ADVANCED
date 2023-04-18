72e32ed13b79cbfc3975ca717769f2e00eb8e036
Bobi <bobi.bg@gmail.com>
2023-04-18 23:27:14 +0300

    18:04:2023

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.stream.Stream;
+
 public class TestClass {
     public static void main(String[] args) {
 
+
+
+
+    }
+
+
+
+    interface NumberAddition {
+        int addition ( int a,int b);
     }
 }
+
