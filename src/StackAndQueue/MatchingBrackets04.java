fc98341e29b6689c0fb6691639028460040ae20c
Bobi <bobi.bg@gmail.com>
2023-04-20 23:01:48 +0300

    20:04:2023

--- src/StackAndQueue/MatchingBrackets04.java
+++ src/StackAndQueue/MatchingBrackets04.java
@@ -1,7 +1,31 @@
 package StackAndQueue;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class MatchingBrackets04 {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < input.length(); i++) {
+
+            char ch = input.charAt(i);
+
+            if (ch == '(') {
+                stack.push(i);
+            } else if (ch == ')') {
+                int startIndex = stack.pop();
+                String contents = input.substring(startIndex, i + 1);
+                System.out.println(contents);
+            }
+
+        }
 
     }
 }
---
+++ src/StackAndQueue/PrinterQueue05.java
@@ -0,0 +1,19 @@
+package StackAndQueue;
+
+import java.util.Scanner;
+
+public class PrinterQueue05 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+
+        while (!input.equals("print")){
+
+
+
+            input = scan.nextLine();
+        }
+    }
+}
