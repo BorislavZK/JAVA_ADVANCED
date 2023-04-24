930d209eb27423e43e3ee7cb058d6e7f322a9822
Bobi <bobi.bg@gmail.com>
2023-04-24 22:02:51 +0300

    24:04:2023

--- src/StackAndQueue/_E_05_BalancedParentheses.java
+++ src/StackAndQueue/_E_05_BalancedParentheses.java
@@ -1,4 +1,11 @@
 package StackAndQueue;
 
+import java.util.Scanner;
+
 public class _E_05_BalancedParentheses {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
 }
