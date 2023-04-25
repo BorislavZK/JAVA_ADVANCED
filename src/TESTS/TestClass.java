72de0826273d8deb0a06ae738145f063a0f04f73
Bobi <bobi.bg@gmail.com>
2023-04-25 20:49:43 +0300

    25:04:2023

--- src/TESTS/TestClass.java
+++ src/TESTS/TestClass.java
@@ -1,29 +1,25 @@
 package TESTS;
-
 import java.util.*;
-
 public class TestClass {
 
     public static void main(String[] args) {
 
-
-        Deque<String> queue = new ArrayDeque<>();
+        ArrayDeque<String> queue = new ArrayDeque<>();
 
         queue.offer("A");
         queue.offer("B");
         queue.offer("C");
+        queue.offer("D");
+        queue.offer("E");
 
-        System.out.println(queue.removeLast());
+        //System.out.println(queue.removeLast());
 
+        System.out.println(queue.poll());
 
+        System.out.println();
 
 
     }
 
-
-
-    interface NumberAddition {
-        int addition ( int a,int b);
-    }
 }
 
