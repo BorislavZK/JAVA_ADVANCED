b0cec38eb21a7f8934036fe2ac219a67fc36f999
Bobi <bobi.bg@gmail.com>
2023-05-22 23:55:45 +0300

    22:05:2023

--- src/ClassesAndObjects/CarSalesman/Engine.java
+++ src/ClassesAndObjects/CarSalesman/Engine.java
@@ -1,4 +1,53 @@
 package ClassesAndObjects.CarSalesman;
 
 public class Engine {
+
+    //An Engine has a model, power, displacement, and efficiency.
+    //Engine displacements, and efficiency are optional
+    //V8-101 220 50
+    //V4-33 140 28 B
+
+
+    private String engineModel;
+    private int enginePower;
+    private int engineDisplacement;
+    private String engineEfficiency;
+
+
+    public Engine(String engineModel, int enginePower) {
+        this.engineModel = engineModel;
+        this.enginePower = enginePower;
+    }
+
+    public String getEngineModel() {
+        return engineModel;
+    }
+
+    public void setEngineModel(String engineModel) {
+        this.engineModel = engineModel;
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+
+    public void setEnginePower(int enginePower) {
+        this.enginePower = enginePower;
+    }
+
+    public int getEngineDisplacement() {
+        return engineDisplacement;
+    }
+
+    public void setEngineDisplacement(int engineDisplacement) {
+        this.engineDisplacement = engineDisplacement;
+    }
+
+    public String getEngineEfficiency() {
+        return engineEfficiency;
+    }
+
+    public void setEngineEfficiency(String engineEfficiency) {
+        this.engineEfficiency = engineEfficiency;
+    }
 }
