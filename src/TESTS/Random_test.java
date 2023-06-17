17aa10461bd4127a82cd45b7e1ce7ef27547977c
Bobi <bobi.bg@gmail.com>
2023-06-17 13:20:44 +0300

    17.06.2023

---
+++ src/TESTS/Random_test.java
@@ -0,0 +1,12 @@
+package TESTS;
+
+public class Random_test {
+    public static void main(String[] args) {
+
+        int randNum =  (int)(Math.random() * 11);
+        System.out.println(randNum);
+
+
+
+    }
+}
